a830d9a67d19626d06cc959406a87d542a53a7b4
Abhishek Kumar Prajapti <119518824+Abhish-akp@users.noreply.github.com>
2024-11-25 16:02:44 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (94.79%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -16,24 +16,36 @@ public:
             return {};
         }
         vector<int>res;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* node = q.front();
-                q.pop();
-                if(i==size-1){
-                    res.push_back(node->val);
-                }
-                if(node->left!=NULL){
-                    q.push(node->left);
-                }
-                if(node->right!=NULL){
-                    q.push(node->right);
-                }
-            }
-        }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         if(i==size-1){
+        //             res.push_back(node->val);
+        //         }
+        //         if(node->left!=NULL){
+        //             q.push(node->left);
+        //         }
+        //         if(node->right!=NULL){
+        //             q.push(node->right);
+        //         }
+        //     }
+        // }
+        function(root,1,res);
         return res;
+    }
+private:
+    void function(TreeNode* root,int value,vector<int>&res){
+        if(root==NULL){
+            return ;
+        }
+        if(res.size()<value){
+            res.push_back(root->val);
+        }
+        function(root->right,value+1,res);
+         function(root->left,value+1,res);
     }
 };
