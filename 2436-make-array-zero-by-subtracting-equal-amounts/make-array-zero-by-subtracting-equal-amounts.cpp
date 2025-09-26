0dccf633c8059c93a8df2169bca0a4804dc0c809
Abhishek Kumar Prajapti <119518824+Abhish-akp@users.noreply.github.com>
2025-09-26 15:11:25 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (70.09%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
-    int non_zero(vector<int>nums){
-        int z=INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<z && nums[i]!=0){
-                z=nums[i];
-            }
-        }
-        return z;
-    }
     int minimumOperations(vector<int>& nums) {
-        int n= nums.size();
-        int count=0;
-        while(1){
-            int num = non_zero(nums);
-            if(num==INT_MAX) break;
-            for(int i=0;i<n;i++){
-                if(nums[i]!=0)
-                    nums[i]=nums[i]-num;
-            }
-            count++;
+        set<int> s;
+        for (auto& num: nums) {
+            if (num > 0) s.insert(num);
         }
-        return count;
+        return s.size();
+        
     }
 };
